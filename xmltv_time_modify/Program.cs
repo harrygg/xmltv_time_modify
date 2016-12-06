d73b32071994dd049d735481ac746227e10dc74a
Harry G <harrygg@gmail.com>
2016-12-06 15:31:42 +0200

    Add project files.

---
+++ xmltv_time_modify/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmltv_time_modify
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
